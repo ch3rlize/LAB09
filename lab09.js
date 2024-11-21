e0647d8dfb6d5b4c7ff53c7f3d2533fba49cebd9
ch3rlize <cnr29@njit.edu>
2024-11-21 10:31:31 -0500

    q6

--- lab09.js
+++ lab09.js
@@ -5,7 +5,7 @@ console.log("Ron")
 console.log("Draco");
 console.log("Luna");
 
-let firstname="Harry";
+let firstName="Harry";
 let house='Welcome ${firstname} Gryffindor,!';
 console.log("house")
 
@@ -21,3 +21,6 @@ console.log("trim()question:", `${spell.trim()}`);
 let quote="I solemnly swear that I am up to no good"
 console.log("slice(11,17):", quote.slice(11,17));
 
+let firstname= "Ron"
+let lastname = "Weasly";
+let combined=firstname.concat('',lastname);
