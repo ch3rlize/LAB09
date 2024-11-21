a89340a6733bf27b82c1a9a12e8b4999e5cb3514
ch3rlize <cnr29@njit.edu>
2024-11-21 10:14:48 -0500

    q2

---
+++ lab09.js
@@ -0,0 +1,10 @@
+let arr = ["Harry","Hermione","Ron","Draco","Luna"]
+console.log("Harry");
+console.log("Hermione");
+console.log("Ron")
+console.log("Draco");
+console.log("Luna");
+
+let firstname="Harry";
+let house='Welcome ${firstname} Gryffindor,!';
+console.log("house")
